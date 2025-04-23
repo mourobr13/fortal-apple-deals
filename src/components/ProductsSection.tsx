a9db8942a6216cfa10e8cde019c60bb16645d679
mourobr13 <agildosegundo@hotmail.com>
2025-04-23 09:35:13 -0300

    Update ProductsSection.tsx

--- src/components/ProductsSection.tsx
+++ src/components/ProductsSection.tsx
@@ -140,10 +140,11 @@ export const ProductsSection = () => {
             
         <div>
           <div className="container mx-auto">
-          <h2 className="text-3xl md:text-4xl font-bold mb-4">Como Funciona nossa sistemática de Trabalho:</h2>
-          <p className="max-w-2xl text-center mx-auto mb-8 text-gray-600">
-            Forma simples, descomplicada e segura de comprar seu produto dos sonhos diretamente dos Estados Unidos, com o menor valor do mercado e tratando diretamente com atendentes brasileiro(a) e zero burocracia.
-          </p>
+            <div className="text-center mb-10">
+             <h2 className="text-3xl md:text-4xl font-bold mb-4">Como Funciona nossa sistemática de Trabalho:</h2>
+             <p className="max-w-2xl text-center mx-auto mb-8 text-gray-600">
+              Forma simples, descomplicada e segura de comprar seu produto dos sonhos diretamente dos Estados Unidos, com o menor valor do mercado e tratando diretamente com atendentes brasileiro(a) e zero burocracia.
+             </p>
           <StepsBox />
         </div>
 
